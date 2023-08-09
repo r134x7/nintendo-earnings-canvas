239c29bd63c22572edc7725d73d9dc70e1195a55
r134x7 <r134x7@gmx.com>
2023-08-09 12:56:05 +1000

    failed attempt at printing text...

--- src/scenes/opening.tsx
+++ src/scenes/opening.tsx
@@ -4,7 +4,7 @@ import { createRef, createSignal, DEFAULT, waitFor, all, tween, Thread, ThreadGe
 
 export default makeScene2D(function* (view) {
 
-    const openingText = "Hello World".split("");
+    const openingText = "welcome to ggx2ac + archives and blah blah blah or other stuff";
 
     // const textBoxFill: string = Array(openingText.length).fill("").join();
 
@@ -28,8 +28,9 @@ export default makeScene2D(function* (view) {
                 ref={getText}
                 text={() => `${openingTextSignal()}`}
                 textWrap={"pre"}
-                // lineHeight={"150%"}
                 fill={"white"}
+                marginLeft={8}
+                marginRight={8}
             />
             </Rect>
         </Rect>
@@ -37,23 +38,84 @@ export default makeScene2D(function* (view) {
 
     yield* loop(
         openingText.length,
-        i => singleMessage(openingText, openingTextSignal, numberSignal, 0.17)
+        // i => singleLineMessage(openingText, openingTextSignal, numberSignal, 0.17)
+        i => textBlock(openingText, 40, openingTextSignal, numberSignal, 0.07)
     )
 
 
 })
 
-function* singleMessage(text: string[], getSignal: SimpleSignal<string, void>, getIndex: SimpleSignal<number, void>, delay: number): ThreadGenerator {
+function* singleLineMessage(text: string, getSignal: SimpleSignal<string, void>, getIndex: SimpleSignal<number, void>, delay: number): ThreadGenerator {
     yield* waitFor(delay)
 
-    const charRange = text.join("").slice(0, getIndex() + 1)
+    const charRange = text.slice(0, getIndex() + 1)
 
-    // getSignal(getSignal() + text[getIndex()] + " ".repeat(text.length - getSignal.length))
-
-    // getSignal((charRange + "_".repeat(text.length - charRange.length)))
     getSignal((charRange + " ".repeat(text.length - charRange.length)))
 
-    // getSignal((charRange + "       _"))
+    getIndex(getIndex() + 1)
+
+    if (text.length === getIndex()) {
+        yield* waitFor(2)
+    }
+}
+
+function* textBlock(text: string, lineLength: number, getSignal: SimpleSignal<string, void>, getIndex: SimpleSignal<number, void>, delay: number): ThreadGenerator {
+
+    yield* waitFor(delay)
+
+    const charRange = text.slice(0, getIndex() + 1)
+
+    // const newLines = "\n".repeat(text.length / lineLength)
+
+    function makeTextBlock(getText: string, newLines: number, getLineLength: number): string {
+
+        let newLineCheck = 0;
+
+        const wordSplit = getText.split(" ")
+            .reduce((acc, next, index, array) => {
+
+                let nextCheck = acc + " " + next + " ";
+
+                if (nextCheck.length > getLineLength) {
+                    newLineCheck++
+                    return acc + " " + next + "\n"
+                } else if (array[index] === next) {
+
+                    // console.log(next);
+                    
+                    let lastLine = acc.length + next.length;
+                    
+                    let repeatValue = ((getText.length + newLineCheck) - lastLine >= 0)
+                        ? (getText.length + newLineCheck) - lastLine
+                        : 0
+
+                    // console.log(getText.length + newLineCheck - lastLine);
+                    
+
+                    // return acc + " " + next + " ".repeat((getText.length + newLineCheck) - lastLine)
+                    return acc + " " + next + " ".repeat(repeatValue)
+                } else {
+                    return acc + " " + next
+                }
+            }, "")
+
+        
+        return wordSplit;
+    }
+
+    // const stringResult = makeTextBlock(charRange, Math.floor(text.length / lineLength), lineLength)
+    const stringResult = makeTextBlock(charRange, Math.floor(text.length / lineLength), 30)
+
+    // getSignal(charRange + " ".repeat(text.length - charRange.length) + newLines)
+    getSignal(stringResult)
+
+    /*
+    ............          \n
+                          \n
+
+    ......................\n
+                          \n
+    */
 
     getIndex(getIndex() + 1)
 
