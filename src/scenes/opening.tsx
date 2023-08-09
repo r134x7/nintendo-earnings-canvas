8ebc695d5dc27ff145e9b126d3c185720e6c2753
r134x7 <r134x7@gmx.com>
2023-08-09 10:17:00 +1000

    signal doesn't seem to allow blank spaces...

--- src/scenes/opening.tsx
+++ src/scenes/opening.tsx
@@ -1,92 +1,61 @@
-import { Txt, makeScene2D } from "@motion-canvas/2d";
+import { Txt, makeScene2D, Rect } from "@motion-canvas/2d";
 
 import { createRef, createSignal, DEFAULT, waitFor, all, tween, Thread, ThreadGenerator, SimpleSignal, delay, loop, sequence, chain } from "@motion-canvas/core";
 
 export default makeScene2D(function* (view) {
 
+    const openingText = "Hello World".split("");
+
+    // const textBoxFill: string = Array(openingText.length).fill("").join();
+
     const openingTextSignal = createSignal("");
     const numberSignal = createSignal(0);
 
-    const openingText = "Hello World".split("");
-
     const getText = createRef<Txt>();
 
+    const textBox = createRef<Rect>();
+
     view.add(
-        <Txt 
-            ref={getText}
-            text={() => `${openingTextSignal()} and ${numberSignal()}`}
-            lineHeight={"150%"}
-        />
+        <Rect 
+            padding={10}
+            radius={20}
+            fill={"white"}
+            layout
+        >
+            <Rect fill={"black"}>
+            <Txt 
+                ref={getText}
+                text={() => `${openingTextSignal()}`}
+                lineHeight={"150%"}
+                fill={"white"}
+            />
+            </Rect>
+        </Rect>
     )
 
     yield* loop(
         openingText.length,
-        i => singleMessage(openingText, openingTextSignal, numberSignal, 1)
+        i => singleMessage(openingText, openingTextSignal, numberSignal, 0.17)
     )
 
-    // yield* delay(
-    //     2,
-    //     loop(
-    //         openingText.length,
-    //         i => {
-    //             openingTextSignal(openingTextSignal() + openingText[i])
-    //         }
-    //     )
-    // )
-    // yield* all(
-    //     waitFor(1),
-    //     loop(
-    //         openingText.length,
-    //         i => {
-    //             waitFor(100000)
-    //             openingTextSignal(openingTextSignal() + openingText[i])
-    //         } 
-    //     )
-    // ) 
-    // yield* loop(
-    //         openingText.length,
-    //         i => delay(5, getText().text(openingText[i]))
-    // )
-    // yield* waitFor(5)
-
-    // yield* waitFor(1)
-    // openingTextSignal(openingTextSignal() + openingText[numberSignal()])
-    // numberSignal(numberSignal() + 1)
-    // yield* waitFor(1)
-    // openingTextSignal(openingTextSignal() + openingText[numberSignal()])
-    // numberSignal(numberSignal() + 1)
-    // yield* waitFor(1)
-    // openingTextSignal(openingTextSignal() + openingText[numberSignal()])
-    // numberSignal(numberSignal() + 1)
-    // yield* waitFor(1)
-    // openingTextSignal(openingTextSignal() + openingText[numberSignal()])
-    // numberSignal(numberSignal() + 1)
-
-    // yield* delay(0.7, numberSignal(3))
-    // yield* waitFor(1)
-    // openingTextSignal("e")
-
-    // yield* waitFor(1)
-    // openingTextSignal(openingTextSignal() + openingText.pop())
-    // yield* waitFor(2)
-
-    // yield* all(
-    //     ...openingText.map((elem, index, array) => {
-    //         // yield waitFor(0.6);
-    //         openingTextSignal(elem)
-    //     })
-    // )
-    // yield* numberSignal(numberSignal() + 10, 3)
-    // yield* openingTextSignal("3", 3)
-    // yield* openingText.map((elem, index, array) => {
-    //     waitFor(1)
-    //     openingTextSignal(openingTextSignal() + elem)
-    // })
 
 })
 
 function* singleMessage(text: string[], getSignal: SimpleSignal<string, void>, getIndex: SimpleSignal<number, void>, delay: number): ThreadGenerator {
     yield* waitFor(delay)
-    getSignal(getSignal() + text[getIndex()])
+
+    const charRange = text.join("").slice(0, getIndex() + 1)
+
+    // getSignal(getSignal() + text[getIndex()] + " ".repeat(text.length - getSignal.length))
+
+    // getSignal((charRange + "_".repeat(text.length - charRange.length)))
+    getSignal((charRange + " ".repeat(text.length - charRange.length)))
+
+    // getSignal((charRange + "       _"))
+
     getIndex(getIndex() + 1)
+
+    if (text.length === getIndex()) {
+        yield* waitFor(2)
+    }
 }
