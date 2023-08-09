511de9607e5808cb2084df4dd72af2fbf4dcef2f
r134x7 <r134x7@gmx.com>
2023-08-10 01:58:11 +1000

    adding movements to svg

--- src/scenes/firstSVG.tsx
+++ src/scenes/firstSVG.tsx
@@ -1,5 +1,5 @@
 import { makeScene2D, Img } from "@motion-canvas/2d";
-import { createRef, waitFor } from "@motion-canvas/core";
+import { Direction, all, createRef, slideTransition, waitFor } from "@motion-canvas/core";
 
 import something from "../assets/episode1/rect3.svg"
 import something2 from "../assets/episode1/firstDrawing.svg"
@@ -14,6 +14,16 @@ export default makeScene2D(function* (view) {
             src={something2}
         />
     )
+
+    yield* slideTransition(Direction.Right);
+
+    // yield* ref().x(-200, 1)
+    yield* all(
+        ref().x(-200, 1),
+        ref().y(200, 1)
+    )
+
+    yield* slideTransition(Direction.Right);
 
     yield* waitFor(4)
 })
