604f0970af0aecc93efe7859613e298bc0d4cae0
r134x7 <r134x7@gmx.com>
2023-08-10 04:54:44 +1000

    first animated bar graph

--- src/scenes/firstSVG.tsx
+++ src/scenes/firstSVG.tsx
@@ -12,13 +12,20 @@ export default makeScene2D(function* (view) {
     const ref = createRef<Img>();
     const getText = createRef<Txt>();
 
+    const bar1 = createRef<Rect>();
+    const bar2 = createRef<Rect>();
+    const value1 = createRef<Txt>();
+    const value2 = createRef<Txt>();
+
     const textSignal = createSignal("");
     const numberSignal = createSignal(0);
 
     const lines = new Map<number, string>();
 
-    lines.set(lines.size, "This is just an avatar.")
-    lines.set(lines.size, "The following data covers the 1st Quarter earnings release of Nintendo for the fiscal year ending March 2024 (FY3/2024).")
+    lines.set(lines.size, "This, is an avatar and I am bad at drawing.")
+    lines.set(lines.size, "The following data from my webpage (link in the description) covers the 1st Quarter earnings release of Nintendo for the fiscal year ending March 2024 (FY3/2024).")
+    lines.set(lines.size, "Consolidated Operating Results.")
+    lines.set(lines.size, "Nintendo's consolidated net sales for the first quarter was ¥461,341M. A +50.05 increase year-over-year.")
 
     view.add(
         <Img 
@@ -84,4 +91,60 @@ export default makeScene2D(function* (view) {
         lines.get(1).length,
         i => textBlock(lines.get(1), 40, textSignal, numberSignal, 0.07, 2)
     )
+
+    textSignal(DEFAULT)
+    numberSignal(DEFAULT)
+
+    yield* loop(
+        lines.get(2).length,
+        i => textBlock(lines.get(2), lines.get(2).length + 1, textSignal, numberSignal, 0.07, 2)
+    )
+
+    textSignal(DEFAULT)
+    numberSignal(DEFAULT)
+
+    yield* ref().x(-650,1)
+    ref().src(avatarB)
+    yield* ref().x(-800,1)
+    ref().src(avatarA)
+
+    yield* loop(
+        lines.get(3).length,
+        i => textBlock(lines.get(3), 40, textSignal, numberSignal, 0.07, 2)
+    )
+
+    const scale = 100;
+
+    view.add(
+        <>
+            <Rect 
+                ref={bar1}
+                height={0}
+                width={100}
+                fill={"red"}
+                x={-500}
+            />
+            <Txt 
+                ref={value1}
+                text={""}
+                fill={"white"}
+                x={-500}
+            />
+        </>
+    )
+    
+    const netSalesFY = 461341;
+    const netSalesLY = 307460;
+
+    // yield* bar1().height(50, 1)
+    yield* all (
+        bar1().height(300, 1),
+        value1().height(400, 1),
+        value1().text("¥461,341M", 1),
+    ) 
+
+    // textSignal(DEFAULT)
+    // numberSignal(DEFAULT)
+
+    yield* waitFor(8)
 })
