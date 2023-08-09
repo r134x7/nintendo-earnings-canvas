702f91a6d64ce59450ff2eda646c270a6609698c
r134x7 <r134x7@gmx.com>
2023-08-10 03:40:14 +1000

    introduction of the avatar, did some movements

--- src/scenes/firstSVG.tsx
+++ src/scenes/firstSVG.tsx
@@ -1,5 +1,7 @@
-import { makeScene2D, Img, Circle } from "@motion-canvas/2d";
-import { Direction, all, createRef, slideTransition, waitFor } from "@motion-canvas/core";
+import { makeScene2D, Img, Circle, Rect, Txt } from "@motion-canvas/2d";
+import { Direction, all, createRef, createSignal, slideTransition, waitFor, loop } from "@motion-canvas/core";
+
+import { textBlock } from "./opening";
 
 import robot from "../assets/episode1/firstDrawing.svg"
 import avatarA from "../assets/episode1/avatar1a.svg";
@@ -8,6 +10,14 @@ import avatarB from "../assets/episode1/avatar1b.svg";
 export default makeScene2D(function* (view) {
 
     const ref = createRef<Img>();
+    const getText = createRef<Txt>();
+
+    const textSignal = createSignal("");
+    const numberSignal = createSignal(0);
+
+    const lines = new Map<number, string>();
+
+    lines.set(lines.size, "This is just a random avatar.")
 
     view.add(
         <Img 
@@ -18,7 +28,6 @@ export default makeScene2D(function* (view) {
 
     yield* slideTransition(Direction.Right);
 
-    // yield* ref().x(-200, 1)
     yield* all(
         ref().x(-800, 1),
         ref().y(300, 1)
@@ -34,4 +43,37 @@ export default makeScene2D(function* (view) {
     yield* waitFor(2)
     ref().src(avatarA)
     yield* waitFor(2)
+
+    view.add(
+        <Rect 
+            padding={10}
+            radius={15}
+            fill={"white"}
+            layout
+            y={200}
+        >
+            <Rect fill={"black"}>
+            <Txt
+                fontFamily={"Consolas"}
+                ref={getText}
+                text={() => `${textSignal()}`}
+                textWrap={"pre"}
+                fill={"white"}
+                marginLeft={16}
+                marginRight={16}
+            />
+            </Rect>
+        </Rect>
+    )
+
+
+    yield* loop(
+        lines.get(0).length,
+        i => textBlock(lines.get(0), 40, textSignal, numberSignal, 0.07, 2)
+    )
+
+    yield* ref().x(-650,1)
+    ref().src(avatarB)
+    yield* ref().x(-800,1)
+    ref().src(avatarA)
 })
