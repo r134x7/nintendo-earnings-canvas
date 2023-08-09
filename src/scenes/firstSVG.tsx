9f5be63ce99fec4ca55eeeb3ebf2c1340e151cbc
r134x7 <r134x7@gmx.com>
2023-08-10 05:28:28 +1000

    adding operating income visuals, figuring out how to change height in one direction

--- src/scenes/firstSVG.tsx
+++ src/scenes/firstSVG.tsx
@@ -11,11 +11,14 @@ export default makeScene2D(function* (view) {
 
     const ref = createRef<Img>();
     const getText = createRef<Txt>();
+    const textBox = createRef<Txt>();
 
     const bar1 = createRef<Rect>();
     const bar2 = createRef<Rect>();
     const value1 = createRef<Txt>();
+    const xValue1 = createRef<Txt>();
     const value2 = createRef<Txt>();
+    const xValue2 = createRef<Txt>();
 
     const textSignal = createSignal("");
     const numberSignal = createSignal(0);
@@ -26,6 +29,7 @@ export default makeScene2D(function* (view) {
     lines.set(lines.size, "The following data from my webpage (link in the description) covers the 1st Quarter earnings release of Nintendo for the fiscal year ending March 2024 (FY3/2024).")
     lines.set(lines.size, "Consolidated Operating Results.")
     lines.set(lines.size, "Nintendo's consolidated net sales for the first quarter was ¥461,341M. A +50.05 increase year-over-year.")
+    lines.set(lines.size, "Nintendo's consolidated operating income for the first quarter was ¥185,441M. A +82.44% increase year-over-year.")
 
     view.add(
         <Img 
@@ -54,6 +58,7 @@ export default makeScene2D(function* (view) {
 
     view.add(
         <Rect 
+            ref={textBox}
             padding={10}
             radius={15}
             fill={"white"}
@@ -120,6 +125,7 @@ export default makeScene2D(function* (view) {
             <Rect 
                 ref={bar1}
                 height={0}
+                minHeight={0}
                 width={100}
                 fill={"red"}
                 x={-500}
@@ -130,21 +136,66 @@ export default makeScene2D(function* (view) {
                 fill={"white"}
                 x={-500}
             />
+            <Txt 
+                ref={xValue1}
+                text={""}
+                fill={"white"}
+                x={-500}
+            />
         </>
     )
     
-    const netSalesFY = 461341;
-    const netSalesLY = 307460;
-
     // yield* bar1().height(50, 1)
     yield* all (
         bar1().height(300, 1),
-        value1().height(400, 1),
+        value1().y(-180, 1),
+        xValue1().y(200, 1),
         value1().text("¥461,341M", 1),
+        xValue1().text("Net Sales", 1),
+        textBox().y(350, 1)
     ) 
 
-    // textSignal(DEFAULT)
-    // numberSignal(DEFAULT)
+    textSignal(DEFAULT)
+    numberSignal(DEFAULT)
+
+    yield* loop(
+        lines.get(4).length,
+        i => textBlock(lines.get(4), 40, textSignal, numberSignal, 0.07, 2)
+    )
+
+    view.add(
+        <>
+            <Rect 
+                ref={bar2}
+                height={0}
+                width={100}
+                fill={"red"}
+                x={-200}
+            />
+            <Txt 
+                ref={value2}
+                text={""}
+                fill={"white"}
+                x={-200}
+            />
+            <Txt 
+                ref={xValue2}
+                text={""}
+                fill={"white"}
+                x={-200}
+            />
+        </>
+    )
+
+    yield* all (
+        bar2().height(300 * .40, 1),
+        bar2().y(90,1),
+        value2().y(-180, 1),
+        xValue2().y(200 * .40, 1),
+        value2().text("¥185,441M", 1),
+        xValue2().text("Operating\nIncome", 1),
+        textBox().y(350, 1)
+    ) 
 
     yield* waitFor(8)
 })
