5eb33d5ca978bdbaedc92aba7767aef2141e9225
r134x7 <r134x7@gmx.com>
2023-08-10 14:32:07 +1000

    setting up fifth scene

---
+++ src/scenes/fifthScene.tsx
@@ -0,0 +1,109 @@
+import { makeScene2D, Img, Rect, Txt } from "@motion-canvas/2d";
+import { Direction, all, createRef, createSignal, slideTransition, waitFor, loop, DEFAULT } from "@motion-canvas/core";
+
+import { textBlock } from "./opening";
+
+import avatarA from "../assets/episode1/avatar1a.svg";
+import avatarB from "../assets/episode1/avatar1b.svg";
+
+export default makeScene2D(function* (view) {
+
+    const ref = createRef<Img>();
+    const getText = createRef<Txt>();
+    const textBox = createRef<Txt>();
+
+    const textSignal = createSignal("");
+    const numberSignal = createSignal(0);
+
+    const lines = new Map<number, string>();
+    
+    lines.set(lines.size, "Final section")
+    lines.set(lines.size, "Topics that weren't covered in this video include: Key Sales Indicators, Regional Hardware/Software Units and, the rest of the Consolidated Sales Information.")
+    lines.set(lines.size, "To find those topics mentioned and more in-depth data, head to my webpage: ggx2ac + archives: Nintendo earnings data and other video game companies. (link in the description)")
+    lines.set(lines.size, "If you want to find more video game sales topics to read, visit the Install Base Forum (link in the description).")
+
+    view.add(
+        <Img 
+            ref={ref}
+            src={avatarA}
+        />
+    )
+
+    yield* slideTransition(Direction.Right);
+
+    yield* all(
+        ref().x(-800, 1),
+        ref().y(300, 1)
+    )
+    yield* waitFor(1)
+
+    yield* slideTransition(Direction.Right);
+
+    ref().fill("white")
+    yield* waitFor(2)
+    ref().src(avatarB)
+    yield* ref().scale(2, 1)
+    yield* waitFor(2)
+    ref().src(avatarA)
+    yield* waitFor(2)
+
+    view.add(
+        <Rect 
+            ref={textBox}
+            padding={10}
+            radius={15}
+            fill={"white"}
+            layout
+            y={250}
+        >
+            <Rect fill={"black"}>
+            <Txt
+                fontFamily={"Consolas"}
+                ref={getText}
+                text={() => `${textSignal()}`}
+                textWrap={"pre"}
+                fill={"white"}
+                marginLeft={16}
+                marginRight={16}
+            />
+            </Rect>
+        </Rect>
+    )
+
+    yield* loop(
+        lines.get(0).length,
+        i => textBlock(lines.get(0), 40, textSignal, numberSignal, 0.07, 2)
+    )
+
+    yield* ref().x(-650,1)
+    ref().src(avatarB)
+    yield* ref().x(-800,1)
+    ref().src(avatarA)
+
+    textSignal(DEFAULT)
+    numberSignal(DEFAULT)
+
+    yield* loop(
+        lines.get(1).length,
+        i => textBlock(lines.get(1), 40, textSignal, numberSignal, 0.07, 2)
+    )
+
+    textSignal(DEFAULT)
+    numberSignal(DEFAULT)
+
+    yield* loop(
+        lines.get(2).length,
+        i => textBlock(lines.get(2), 40, textSignal, numberSignal, 0.07, 2)
+    )
+    textSignal(DEFAULT)
+    numberSignal(DEFAULT)
+
+    yield* loop(
+        lines.get(3).length,
+        i => textBlock(lines.get(3), 40, textSignal, numberSignal, 0.07, 2)
+    )
+    textSignal(DEFAULT)
+    numberSignal(DEFAULT)
+
+    yield* waitFor(4)
+})
