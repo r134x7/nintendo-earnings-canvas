fcc123b5a89f7b1d74d4007c78170881e4343468
r134x7 <r134x7@gmx.com>
2023-10-07 02:36:18 +1100

    finished exposition dump

--- src/scenes/episode3/act1.tsx
+++ src/scenes/episode3/act1.tsx
@@ -132,14 +132,31 @@ export default makeScene2D(function* (view) {
 
     lines.set(lines.size, `What they refer to as a Multifaceted Rollout in the report to show which areas their IP are used in, I will be referring to it as platforms.`);
 
-    // lines.set(lines.size, ``);
     lines.set(lines.size, `From the Fiscal Year Ending March 2014 (FY3/2014), the Sonic the Hedgehog IP has been used in the following five platforms: Pachislot and Pachinko Machines, Digital Games, Packaged Games, Amusement Arcade Machines, and Toys. The cumulative units and downloads in FY3/2014 for the Sonic the Hedgehog IP is 80M. Life-To-Date is 150M.`);
 
     lines.set(lines.size, `The Life-To-Date number more than doubled once they expanded out the number of platforms the Sonic the Hedgehog IP is used in. Note: Packaged Games = Home Video Game Software, Digital Games refers to online/smartphone games. You will find this reading through the Integrated Report 2014. That means the Sonic the Hedgehog IP numbers the previous FY came from the Packaged Games platform and then in FY3/2014 they expanded it to the rest.`);
 
     lines.set(lines.size, `The platform names will change over time as I put up the numbers but the Sonic the Hedgehog IP has been used in all of them. Amusement Arcade Machines will change to Amusement Machines. Packaged Games and Digital Games will be combined to form Consumer Games and will then be renamed to Video Games. Toys will be renamed to Toys, etc. Movies will be added as a platform in the Integrated Report 2022.`);
 
-    lines.set(lines.size, ``);
+    lines.set(lines.size, `Integrated Report 2015: The Sonic the Hedgehog IP had cumulative units and downloads of 185M for FY3/2015. Life-To-Date: 335M. Platforms: Pachislot and Pachinko Machines, Digital Games, Packaged Games, Amusement Machines, Toys.`);
 
+    lines.set(lines.size, `Integrated Report 2016: The Sonic the Hedgehog IP had cumulative units and downloads of 15M for FY3/2016. Life-To-Date: 350M. Platforms: Pachislot and Pachinko Machines, Digital Games, Packaged Games, Amusement Machines, Toys.`);
 
+    lines.set(lines.size, `Integrated Report 2017: The Sonic the Hedgehog IP had cumulative units and downloads of 10M for FY3/2017. Life-To-Date: 360M. Platforms: Pachislot and Pachinko Machines, Digital Games, Packaged Games, Amusement Machines, Toys, etc.`);
+
+    lines.set(lines.size, `Integrated Report 2018: A significant change occurred. Sega Sammy included downloads of free-to-play titles to their IP where applicable. In my opinion, downloading a title for free doesn’t clearly show if the IP is strong. To know the value of the IP it would be helpful to know the sales numbers and downloads from the free-to-play titles to then calculate a sales per free-to-play download figure.`);
+
+    lines.set(lines.size, `The free-to-play downloads now end up being combined with the other units and downloads of the Sonic the Hedgehog IP. Cumulative units and downloads* (*including downloads of free-to-play titles) of 440M for FY3/2018. Life-To-Date: 800M. Platforms: Pachislot and Pachinko Machines, Digital Games, Packaged Games, Amusement Machines, Toys, etc.`);
+
+    lines.set(lines.size, `Integrated Report 2019: The Sonic the Hedgehog IP had cumulative units and downloads* (*including downloads of free-to-play titles) of 120M for FY3/2019. Life-To-Date: 920M. Platforms: Pachislot and Pachinko Machines, Digital Games, Packaged Games, Amusement Machines, Toys, etc.`);
+
+    lines.set(lines.size, `Integrated Report 2020: The Sonic the Hedgehog IP had cumulative units and downloads* (*including downloads of free-to-play titles) of 220M for FY3/2020. Life-To-Date: 1140M. Platforms: Pachislot and Pachinko Machines, Consumer Games, Amusement Machines, Toys, etc.`);
+
+    lines.set(lines.size, `Integrated Report 2021: The Sonic the Hedgehog IP had cumulative units and downloads* (*including downloads of free-to-play titles) of 240M for FY3/2021. Life-To-Date: 1380M. Platforms: Pachislot and Pachinko Machines, Consumer Games, Amusement Machines, Toys, etc.`);
+
+    lines.set(lines.size, `Integrated Report 2022: The Sonic the Hedgehog IP had cumulative units and downloads* (*including downloads of free-to-play titles) of 130M for FY3/2022. Life-To-Date: 1510M. Platforms: Pachislot and Pachinko Machines, Video Games, Movies, Amusement Machines, Toys, etc.`);
+
+    lines.set(lines.size, `Integrated Report 2023: The Sonic the Hedgehog IP had cumulative units and downloads* (*including downloads of free-to-play titles) of 150M for FY3/2023. Life-To-Date: 1660M. Platforms: Pachislot and Pachinko Machines, Video Games, Movies, Amusement Machines, Toys, etc.`);
+
+    // lines.set(lines.size, ``);
 })
